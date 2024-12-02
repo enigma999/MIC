46ffc356b82597cd133732b2057d3030c788b52b
ENIGMA <stanborst@gmail.com>
2024-12-02 11:33:25 +0100

    did things

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,46 @@
 #include <Arduino.h>
 #define BAUDRATE 9600
 
+enum bstate
+{
+  pressed,
+  released
+};
 
-void display_counter(uint8_t counter){
-    PORTC = counter;
+void display_counter(uint8_t counter)
+{
+  PORTC = counter;
 }
 
-int main(){
-    DDRC |= (1 << PORTC0) |(1 << PORTC1) |(1 << PORTC2) |(1 << PORTC3);;
+void init()
+{
+  DDRC = 0x0f;
+  PORTC |= (1 << PORTC0) | (1 << PORTC1) | (1 << PORTC2) | (1 << PORTC3);
+  DDRD = 0x00;
+  PORTD |= (1 << PORTD2);
+}
 
-    while(true)
+bool readButton()
+{
+  return !((PIND & 0b00000100) == 0b0000100);
+}
+
+enum bstate button_state(void)
+{
+}
+
+int main()
+{
+  init();
+  while (true)
+  {
+    if (readButton())
     {
-        display_counter(0b00000000);
+      display_counter(0b00001111);
     }
+    else
+    {
+      display_counter(0b00000000);
+    }
+  }
 }
